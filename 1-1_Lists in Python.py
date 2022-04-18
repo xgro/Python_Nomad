883318cc5b6b208eee00890f118f403a1467875c
xgro <zero1401@naver.com>
2022-04-18 20:23:16 +0900

    Challenge Python first day

---
+++ 1-1_Lists in Python.py
@@ -0,0 +1,9 @@
+# https://docs.python.org/3/library/ 파이썬 참조 공식 문서
+# 자료형 연습 list
+days = ["Mon","Tue","Wed","Thur","Fri"]
+
+print(days)
+print(type(days)) 
+print("Mon" in days)
+print(days[3])
+print(len(days))
