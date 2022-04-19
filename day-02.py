a27f260212e8ffa2639df2c23a4bcb8073b224aa
xgro <zero1401@naver.com>
2022-04-19 09:52:07 +0900

    Day 02 of 14 code_challenge

---
+++ day-02.py
@@ -0,0 +1,38 @@
+"""
+As you can see, the code is broken.
+Create the missing functions, use default arguments.
+Sometimes you have to use 'return' and sometimes you dont.
+Start by creating the functions
+"""
+def is_on_list(list,item):
+  if item in list: 
+    return True
+  
+  else:
+    return False
+
+def get_x(list,idx):
+  return list[idx]
+
+def add_x(list, item):
+  list.append(item)
+  
+def remove_x(list, item):
+  list.remove(item)
+
+# \/\/\/\/\/\/\  DO NOT TOUCH AREA  \/\/\/\/\/\/\ #
+
+days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+
+print("Is Wed on 'days' list?", is_on_list(days, "Wed"))
+
+print("The fourth item in 'days' is:", get_x(days, 3))
+
+add_x(days, "Sat")
+print(days)
+
+remove_x(days, "Mon")
+print(days)
+
+
+# /\/\/\/\/\/\/\ END DO NOT TOUCH AREA /\/\/\/\/\/\/\ #
