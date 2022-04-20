b29159fcde9e06146dc4b71c7c76271047c05721
xgro <zero1401@naver.com>
2022-04-20 20:09:25 +0900

    Day 03 of 14 code_challenge

---
+++ day-03.py
@@ -0,0 +1,204 @@
+"""
+Again, the code is broken, you need to create 4 functions.
+  - add_to_dict: Add a word to a dict.
+  - get_from_dict: Get a word from inside a dict.
+  - update_word: Update a word inside of the dict.
+  - delete_from_dict: Delete a word from the dict.
+
+All this functions should check for errors, follow the comments to see all cases you need to cover.
+
+There should be NO ERRORS from Python in the console.
+"""
+
+
+def add_to_dict(dict1, *args):
+  pass
+  global my_english_dict
+
+  #첫번째 판별
+  if type(dict1) is not dict:
+    print(f"You need to send a dictionary. You sent: {type(dict1)}")
+    return
+
+  #두번째 판별  
+  if len(args) < 2:
+    print(f"You need to send a word and a definition.")
+    return
+    
+  #세번째 판별  
+  if args[0] not in my_english_dict:
+    dict1 = {f'{args[0]}': args[1]}
+    print(f"{args[0]} has been added.")
+    my_english_dict = dict1
+    return
+
+  #마지막 판별
+  if args[0] in my_english_dict:
+    print(f"{args[0]} is already on the dictionary. Won't add.")
+    return
+
+def get_from_dict(dict1, *args):
+  pass
+  #첫번째 판별
+  if type(dict1) is not dict:
+    print(f"You need to send a dictionary. You sent: {type(dict1)}")
+    return
+
+  #두번째 판별  
+  if len(args) == 0:
+    print(f"You need to send a word search for.")
+    return
+    
+  #세번째 판별  
+  if args[0] not in dict1:
+    print(f"{args[0]} was not found in this dict.")
+    return
+      
+  #마지막 판별
+  if args[0] in dict1:
+    print(f'{args[0]}: {dict1[args[0]]}')
+    return
+
+def update_word(dict1, *args):
+  pass
+  global my_english_dict
+  
+  #첫번째 판별
+  if type(dict1) is not dict:
+    print(f"You need to send a dictionary. You sent: {type(dict1)}")
+    return
+
+  #두번째 판별  
+  if len(args) < 2:
+    print(f"You need to send a word and a definition to update.")
+    return
+    
+  #세번째 판별  
+  if args[0] not in dict1:
+    print(f"{args[0]} is not on the dict. Can't update non-existing word.")
+    return
+      
+  #마지막 판별
+  if args[0] in dict1:
+    dict1[args[0]] = args[1]
+    print(f'{args[0]} has been updated to: {dict1[args[0]]}')
+    my_english_dict = dict1
+    return
+
+def delete_from_dict(dict1, *args):
+  pass
+  global my_english_dict
+  
+  #첫번째 판별
+  if type(dict1) is not dict:
+    print(f"You need to send a dictionary. You sent: {type(dict1)}")
+    return
+
+  #두번째 판별  
+  if len(args) == 0:
+    print(f"You need to specify a word to delete.")
+    return
+    
+  #세번째 판별  
+  if args[0] not in dict1:
+    print(f"{args[0]} is not on this dict. Won't delete.")
+    return
+      
+  #마지막 판별
+  if args[0] in dict1:
+    del(dict1[args[0]])
+    print(f'{args[0]} has been deleted.')
+    my_english_dict = dict1
+    return
+
+
+
+# \/\/\/\/\/\/\ DO NOT TOUCH  \/\/\/\/\/\/\
+
+import os
+
+os.system('clear')
+
+my_english_dict = {}
+
+print("\n###### add_to_dict ######\n")
+
+# Should not work. First argument should be a dict.
+print('add_to_dict("hello", "kimchi"):')
+add_to_dict("hello", "kimchi")
+
+# Should not work. Definition is required.
+print('\nadd_to_dict(my_english_dict, "kimchi"):')
+add_to_dict(my_english_dict, "kimchi")
+
+# Should work.
+print('\nadd_to_dict(my_english_dict, "kimchi", "The source of life."):')
+add_to_dict(my_english_dict, "kimchi", "The source of life.")
+
+# Should not work. kimchi is already on the dict
+print('\nadd_to_dict(my_english_dict, "kimchi", "My fav. food"):')
+add_to_dict(my_english_dict, "kimchi", "My fav. food")
+
+print("\n\n###### get_from_dict ######\n")
+
+# Should not work. First argument should be a dict.
+print('\nget_from_dict("hello", "kimchi"):')
+get_from_dict("hello", "kimchi")
+
+# Should not work. Word to search from is required.
+print('\nget_from_dict(my_english_dict):')
+get_from_dict(my_english_dict)
+
+# Should not work. Word is not found.
+print('\nget_from_dict(my_english_dict, "galbi"):')
+get_from_dict(my_english_dict, "galbi")
+
+# Should work and print the definiton of 'kimchi'
+print('\nget_from_dict(my_english_dict, "kimchi"):')
+get_from_dict(my_english_dict, "kimchi")
+
+print("\n\n###### update_word ######\n")
+
+# Should not work. First argument should be a dict.
+print('\nupdate_word("hello", "kimchi"):')
+update_word("hello", "kimchi")
+
+# Should not work. Word and definiton are required.
+print('\nupdate_word(my_english_dict, "kimchi"):')
+update_word(my_english_dict, "kimchi")
+
+# Should not work. Word not found.
+print('\nupdate_word(my_english_dict, "galbi", "Love it."):')
+update_word(my_english_dict, "galbi", "Love it.")
+
+# Should work.
+print('\nupdate_word(my_english_dict, "kimchi", "Food from the gods."):')
+update_word(my_english_dict, "kimchi", "Food from the gods.")
+
+# Check the new value.
+print('\nget_from_dict(my_english_dict, "kimchi"):')
+get_from_dict(my_english_dict, "kimchi")
+
+print("\n\n###### delete_from_dict ######\n")
+
+# Should not work. First argument should be a dict.
+print('\ndelete_from_dict("hello", "kimchi"):')
+delete_from_dict("hello", "kimchi")
+
+# Should not work. Word to delete is required.
+print('\ndelete_from_dict(my_english_dict):')
+delete_from_dict(my_english_dict)
+
+# Should not work. Word not found.
+print('\ndelete_from_dict(my_english_dict, "galbi"):')
+delete_from_dict(my_english_dict, "galbi")
+
+# Should work.
+print('\ndelete_from_dict(my_english_dict, "kimchi"):')
+delete_from_dict(my_english_dict, "kimchi")
+
+# Check that it does not exist
+print('\nget_from_dict(my_english_dict, "kimchi"):')
+get_from_dict(my_english_dict, "kimchi")
+
+# \/\/\/\/\/\/\ END DO NOT TOUCH  \/\/\/\/\/\/\
